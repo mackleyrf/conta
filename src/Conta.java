42d32ce151c21530e1d19a702b3a5ec0f86b2542
Mackley Freire <mackleyrf@hotmail.com>
2024-06-11 09:50:33 -0300

    Inicializando dados de cliente.

---
+++ src/Conta.java
@@ -0,0 +1,13 @@
+public class Conta {
+    public static void main(String[] args) {
+        String nome = "João da Silva";
+        String tipoConta = "Corrente";
+        double saldo = 2000.00;
+
+        System.out.println("***************************************\n");
+        System.out.println("Nome do Cliente: " + nome);
+        System.out.println("Tipo de Conta: " + tipoConta);
+        System.out.println("Saldo em Conta: " + saldo);
+        System.out.println("\n***************************************");
+    }
+}
