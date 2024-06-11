5296fc5ab0ae0099a746318544d9c8e62366ac76
Mackley Freire <mackleyrf@hotmail.com>
2024-06-11 11:02:45 -0300

    Menu de opções.

--- src/Conta.java
+++ src/Conta.java
@@ -1,13 +1,32 @@
+import java.util.Scanner;
+
 public class Conta {
     public static void main(String[] args) {
         String nome = "João da Silva";
         String tipoConta = "Corrente";
         double saldo = 2000.00;
+        int opcao = 0;
 
         System.out.println("***************************************\n");
         System.out.println("Nome do Cliente: " + nome);
         System.out.println("Tipo de Conta: " + tipoConta);
         System.out.println("Saldo em Conta: " + saldo);
         System.out.println("\n***************************************");
+
+        String menu = """
+                
+                Digite sua opção:
+                1 - Consultar Saldo
+                2 - Transferir Valor
+                3 - Receber Valor
+                4 - Sair
+                """;
+
+        Scanner leitura = new Scanner(System.in);
+
+        while (opcao != 4) {
+            System.out.println(menu);
+            opcao = leitura.nextInt();
+        }
     }
 }
